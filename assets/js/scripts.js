b69e22288e5b3f7658e97eb9594fd1b43c64dab5
Peter Olesen <63056450+peter-olesen@users.noreply.github.com>
2023-11-25 20:42:32 +0100

    i did a thing

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,1 +1,14 @@
-// Write cool JavaScript below
+const modalContainer = document.getElementsByClassName('container')
+const btnOpen = document.getElementById('btnOpen');
+const btnClose = document.getElementById('btnClose');
+
+
+btnOpen.addEventListener('click', function() {
+    modal.style.display = 'block';
+    container.style.display= 'none';
+});
+   
+btnClose.addEventListener('click', function() {
+    modal.style.display = 'none';
+    container.style.display= 'block';
+});
